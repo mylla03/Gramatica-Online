b41ecf64612bd9c6fc5e2c939c4f2b8d82784014
RUDMYLLA MONTEIRO SOUZA <20231174010026@ifrn.local>
2026-09-09 10:06:56 -0300

    atualizacao no codigo html

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\20231174010026\Gramatica-Online\venv"
+set "VIRTUAL_ENV=D:\Users\20231174010026.IFRN\Gramatica-Online\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
